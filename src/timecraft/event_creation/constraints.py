a11c5678ff8167d7efa5d9d19b3d2d00e65ae0f9
Sajith17 <sajithseelan17@gmail.com>
2024-01-13 17:41:56 +0530

    event creation constraints implementation over

--- src/timecraft/event_creation/constraints.py
+++ src/timecraft/event_creation/constraints.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from enum import Enum
 from typing import List
+from icecream import ic
 
 from timecraft.event_creation.event import Class
 
@@ -18,6 +19,28 @@ class Constraint(ABC):
         pass
 
 
+class HourConstraint(Constraint):
+    def __init__(self):
+        super().__init__(Constraint_type=super().ConstraintType("hard"))
+
+    def calculate_fitness(
+        self, genome: List[List[int]], classes: List[Class], fixed_slots: List[int]
+    ):
+        fitness = 0
+        for row in genome:
+            hours_by_class_index = dict()
+            for class_index in row:
+                if class_index in hours_by_class_index:
+                    hours_by_class_index[class_index] += 1
+                else:
+                    hours_by_class_index[class_index] = 1
+            fitness += sum(
+                max(hours - classes[class_index].no_hours, 0)
+                for class_index, hours in hours_by_class_index.items()
+            )
+        return fitness
+
+
 class FacultyOverlapConstraint(Constraint):
     def __init__(self):
         super().__init__(Constraint_type=super().ConstraintType("hard"))
@@ -33,4 +56,22 @@ class FacultyOverlapConstraint(Constraint):
                 )
                 if faculty_occupied_slots and fixed_slot in faculty_occupied_slots:
                     fitness += 1
+        return fitness
+
+
+class ColumnRedundancyConstraint(Constraint):
+    """This constraint is used to minimize the number of unique columns which then results in minimized number of event creation."""
+
+    def __init__(self):
+        super().__init__(Constraint_type=super().ConstraintType("soft"))
+
+    def calculate_fitness(
+        self, genome: List[List[int]], classes: List[Class], fixed_slots: List[int]
+    ):
+        fitness = len(
+            set(
+                tuple(genome[i][column_index] for i in range(len(genome)))
+                for column_index in range(len(fixed_slots), len(genome[0]))
+            )
+        )
         return fitness
--- tests/test_event_creation_constraints.py
+++ tests/test_event_creation_constraints.py
@@ -1,12 +1,63 @@
 import unittest
 from src.timecraft.models import Faculty
 from src.timecraft.event_creation.event import Class
-from src.timecraft.event_creation.constraints import FacultyOverlapConstraint
+from src.timecraft.event_creation.constraints import (
+    HourConstraint,
+    FacultyOverlapConstraint,
+    ColumnRedundancyConstraint,
+)
 
 from icecream import ic
 
 
 class TestConstraints(unittest.TestCase):
+    def test_hour_constraint(self):
+        classes = [
+            Class(
+                course_code="Math",
+                faculties=[Faculty(code="MATH1", occupied_slots=[2, 3])],
+                no_hours=3,
+            ),
+            Class(
+                course_code="Math",
+                faculties=[Faculty(code="MATH2", occupied_slots=[0, 1])],
+                no_hours=3,
+            ),
+            Class(
+                course_code="Eng",
+                faculties=[Faculty(code="ENG1", occupied_slots=[0, 1])],
+                no_hours=3,
+            ),
+            Class(
+                course_code="Eng",
+                faculties=[Faculty(code="ENG2", occupied_slots=[0, 1])],
+                no_hours=3,
+            ),
+        ]
+        genome = [[0, 0, 1, 0, 1, 1]]
+        fixed_slots = [0, 1, 2]
+        self.assertEqual(
+            HourConstraint().calculate_fitness(
+                genome=genome, classes=classes, fixed_slots=fixed_slots
+            ),
+            0,
+        )
+        genome = [[0, 0, 1, 0, 0, 0]]
+        fixed_slots = [0, 1, 2]
+        self.assertEqual(
+            HourConstraint().calculate_fitness(
+                genome=genome, classes=classes, fixed_slots=fixed_slots
+            ),
+            2,
+        )
+        genome = [[0, 0, 0, 0, 0, 0], [2, 2, 2, 2, 2, 2]]
+        self.assertEqual(
+            HourConstraint().calculate_fitness(
+                genome=genome, classes=classes, fixed_slots=fixed_slots
+            ),
+            6,
+        )
+
     def test_faculty_overlap_constraint_no_overlap(self):
         classes = [
             Class(
@@ -71,6 +122,59 @@ class TestConstraints(unittest.TestCase):
                 genome=genome, classes=classes, fixed_slots=fixed_slots
             ),
             6,
+        )
+
+    def test_column_redundancy_constraint(self):
+        classes = [
+            Class(
+                course_code="Math",
+                faculties=[Faculty(code="MATH1", occupied_slots=[2, 3])],
+                no_hours=3,
+            ),
+            Class(
+                course_code="Math",
+                faculties=[Faculty(code="MATH2", occupied_slots=[0, 1])],
+                no_hours=3,
+            ),
+            Class(
+                course_code="Eng",
+                faculties=[Faculty(code="ENG1", occupied_slots=[0, 1])],
+                no_hours=3,
+            ),
+            Class(
+                course_code="Eng",
+                faculties=[Faculty(code="ENG2", occupied_slots=[0, 1])],
+                no_hours=3,
+            ),
+        ]
+        genome = [[1, 1, 1, 0, 0, 0], [2, 2, 2, 3, 3, 3]]
+        fixed_slots = []
+        self.assertEqual(
+            ColumnRedundancyConstraint().calculate_fitness(
+                genome=genome, classes=classes, fixed_slots=fixed_slots
+            ),
+            2,
+        )
+        genome = [[1, 1, 1, 0, 0, 0], [3, 2, 3, 2, 3, 2]]
+        self.assertEqual(
+            ColumnRedundancyConstraint().calculate_fitness(
+                genome=genome, classes=classes, fixed_slots=fixed_slots
+            ),
+            4,
+        )
+        fixed_slots = [1, 2, 3, 4, 5, 6]
+        self.assertEqual(
+            ColumnRedundancyConstraint().calculate_fitness(
+                genome=genome, classes=classes, fixed_slots=fixed_slots
+            ),
+            0,
+        )
+        fixed_slots = [1, 2, 3]
+        self.assertEqual(
+            ColumnRedundancyConstraint().calculate_fitness(
+                genome=genome, classes=classes, fixed_slots=fixed_slots
+            ),
+            2,
         )
 
 
