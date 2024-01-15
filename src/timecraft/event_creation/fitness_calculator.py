d60460cb156afcf1172b4b300ab6a50ddb301335
Sajith17 <sajithseelan17@gmail.com>
2024-01-15 21:59:20 +0530

    fitness_calculator created

--- src/timecraft/event_creation/fitness_calculator.py
+++ src/timecraft/event_creation/fitness_calculator.py
@@ -7,21 +7,45 @@ class FitnessCalculator:
     def __init__(self, constraints: List[Constraint]):
         self.constraints = constraints
 
-    def calculate_fitness_score(self, assignment: List[List[int]], classes: List[Class], fixed_slots: List[int])->float:
+    def calculate_fitness_score(
+        self, assignment: List[List[int]], classes: List[Class], fixed_slots: List[int]
+    ) -> float:
         fitness_score = 0
-        fitness_score += self.scale(sum(constraint.calculate_fitness_score(assignment=assignment, classes=classes, fixed_slots=fixed_slots) for constraint in self.hard_constraints))
-         
-        
-        
+        fitness_score += self.scale(
+            sum(
+                constraint.calculate_fitness_score(
+                    assignment=assignment, classes=classes, fixed_slots=fixed_slots
+                )
+                for constraint in self.hard_constraints
+            )
+        )
+        if fitness_score == 1:
+            fitness_score += self.scale(
+                sum(
+                    constraint.calculate_fitness_score(
+                        assignment=assignment, classes=classes, fixed_slots=fixed_slots
+                    )
+                    for constraint in self.soft_constraints
+                )
+            )
+        return fitness_score
+
     @property
-    def hard_constraints(self)->List[Constraint]:
-        return [self.constraints for constraint in self.constraints if constraint.Type.value = 'hard']
-    
-    
+    def hard_constraints(self) -> List[Constraint]:
+        return [
+            constraint
+            for constraint in self.constraints
+            if constraint.type.value == "hard"
+        ]
+
     @property
-    def soft_constraints(self)->List[Constraint]:
-        return [self.constraints for constraint in self.constraints if constraint.Type.value = 'soft']
-    
+    def soft_constraints(self) -> List[Constraint]:
+        return [
+            constraint
+            for constraint in self.constraints
+            if constraint.type.value == "soft"
+        ]
+
     @staticmethod
-    def scale(x: int)->float:
-        return 1/(1.0+x) 
+    def scale(x: int) -> float:
+        return 1 / (1.0 + x)
